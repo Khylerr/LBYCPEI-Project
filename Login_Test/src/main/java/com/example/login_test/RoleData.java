f65da78709eea283192461d610fc9541c7a09ced
Khyler Chua <163345852+Khylerr@users.noreply.github.com>
2024-07-25 18:04:31 +0800

    Permission detection

---
+++ Login_Test/src/main/java/com/example/login_test/RoleData.java
@@ -0,0 +1,14 @@
+package com.example.login_test;
+
+public class RoleData {
+
+    private static String role;
+
+    public static String getRole() {
+        return role;
+    }
+
+    public static void setRole(String role) {
+        RoleData.role = role;
+    }
+}
